e0969f6590197be4ff1565166c5d2fd57caa2f9c
root <szwyz@mail.ustc.edu.cn>
2020-11-08 21:24:54 +0800

    A MvvmPriactice

---
+++ MVVMpractice/SimpleMvvmDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleMvvmDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void saveMunuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.ShowDialog();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.slider3.Value = slider1.Value + slider2.Value;
+        }
+    }
+}
